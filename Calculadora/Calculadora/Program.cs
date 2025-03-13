866dfbeccbb9d2334570ac2e863a7c1419339580
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-03-13 08:25:08 -0300

    Inserindo projetos em andamento

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Calc
+    {
+        static void Main(string[] args)
+        {
+            float firstNumber;
+            float secondNumber;
+
+            Console.WriteLine("Informe o primeiro número:");
+            Console.ReadLine(firstNumber);
+            Console.WriteLine("Informe o segundo número:");
+            Console.ReadLine(secondNumber);
+
+
+
+        }
+
+    }
+}
+
+
