f7f9b6af7098c9d8f9b18be71e4f2bb1a91fe0e9
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-03-17 14:03:02 -0300

    Fatorial

---
+++ Fatorial/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main (string[] args)
+        { 
+            Principal();
+        }
+
+        static void Principal()
+        {
+            Console.Clear();
+            Console.Write("Informe o valor que deseja calcular o fatorial: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write($"O fatorial do {number} é  ");
+            Calculo(number);
+        }
+
+        static void Calculo(int number)
+        {
+            int fatorial = 1;
+            if (number > 0)
+            {
+                for (; number > 1; number--)
+                {
+                    fatorial *= number;
+                }
+                Console.Write(fatorial);
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("Inválido.");
+            }
+
+            
+
+        }
+
+    }
+}
