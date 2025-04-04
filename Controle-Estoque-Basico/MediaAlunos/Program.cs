a0e2f90a5d7bda5d3bd5012e1676080b571b203a
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-04-04 16:32:33 -0300

    Sistemas Básicos

---
+++ Controle-Estoque-Basico/MediaAlunos/Program.cs
@@ -0,0 +1,94 @@
+﻿/*Dictionary<string, List<int>> notasAlunos = new Dictionary<string, List<int>>
+{
+    { "Gabriel", new List<int> { 10, 5, 8} },
+    { "Maria", new List<int> {5, 6, 8} }
+
+};
+
+List<int> notasGabriel = notasAlunos["Gabriel"];
+double mediaNotasGabriel = notasGabriel.Average();
+Console.WriteLine($"Media {mediaNotasGabriel:F2}");
+*/
+
+// Dicionario
+
+using System;
+using System.ComponentModel.Design;
+using System.Linq;
+
+Dictionary<string, int> estoqueProdutos = new Dictionary<string,int>();
+Menu();
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("1 - Cadastrar Produto");
+    Console.WriteLine("2 - Informar Estoque do Produto");
+    Console.WriteLine("3 - Verificar Estoque");
+    
+    int opcaoUsuario = int.Parse(Console.ReadLine()!);  
+
+    switch (opcaoUsuario)
+    {
+        case 1: CadastrarProduto(); break;
+        case 2: CadastrarEstoqueProduto(); break;
+        case 3: ImprimirEstoque(); break;
+    }
+    
+}
+
+
+void CadastrarProduto()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o nome do produto a ser cadastrado: ");
+    string nomeProduto = Console.ReadLine()!;
+    estoqueProdutos[nomeProduto] = 0;
+    Console.Clear();
+    Console.WriteLine("Produto cadastro com sucesso.");
+    Thread.Sleep(2000);
+    Menu();
+}
+
+void CadastrarEstoqueProduto ()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o produto que deseja inserir o estoque: ");
+    string produtoUsuario = Console.ReadLine()!;
+
+    if (estoqueProdutos.ContainsKey(produtoUsuario))
+        {
+        Console.WriteLine("Produto encontrado.");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine($"Informe a quantidade no estoque do {produtoUsuario}: ");
+        int quantidadeProduto = int.Parse( Console.ReadLine()!);
+        estoqueProdutos[produtoUsuario] += quantidadeProduto;
+        Console.WriteLine("Quantidade cadastrada.");
+        Thread.Sleep(2000);
+        Menu();
+    }
+
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"{produtoUsuario} não encontrado.");
+        Thread.Sleep(2000);
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
+        Menu();
+    }
+}
+
+void ImprimirEstoque()
+{
+    foreach (var item in estoqueProdutos)
+    {
+        Console.WriteLine($"Produto: {item.Key} \tQuantidade: {item.Value}");
+        Thread.Sleep(2000);
+        Console.WriteLine();
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
+        Menu();
+    }
+}
---
+++ LoginUsandoDicionarioBasico/LoginUsandoDicionario/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+Menu();
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("1 - Login");
+    Console.WriteLine("2 - Cadastre-se");
+    Console.WriteLine("0 - Sair");
+
+    int opcaoUsuario = int.Parse(Console.ReadLine());
+    switch (opcaoUsuario)
+    {
+        case 1: LoginUsuario(); break;
+        case 2: RegistroUsuario(); break;
+        case 0: Console.WriteLine("Saindo..."); Thread.Sleep(1000); break;
+    }
+}
+
+void RegistroUsuario()
+{
+    Console.WriteLine("Cadastro Usuario");
+    // Pedir nome
+    Console.Write("Usuário: ");
+    string usuario = Console.ReadLine()!;
+    usuarios[usuario] = "";
+    Console.Write("Senha: ");
+    string senha = Console.ReadLine()!;
+    usuarios[usuario] += senha;
+
+    Console.WriteLine("Usuário cadastrado com sucesso.");
+    Thread.Sleep(2000);
+    Menu();
+
+}
+void LoginUsuario()
+{
+    Console.WriteLine("Login");
+    Console.Write("Usuário: ");
+    string usuarioLogin = Console.ReadLine()!;
+    Console.Write("Senha: ");
+    string usuarioSenha = Console.ReadLine()!;
+
+        if (usuarios.ContainsKey(usuarioLogin))
+        {
+            if (usuarios[usuarioLogin] == usuarioSenha)
+            {
+                Console.WriteLine($"Login Bem Sucedido. Seja Bem-Vindo");
+            } else 
+            {
+                Console.WriteLine("Senha Inválida!");
+                Thread.Sleep(2000);
+                Menu();
+            }
+        } else
+        {
+            Console.WriteLine("Usuário Inválido");
+            Thread.Sleep(2000);
+            Menu();
+        }
+}    
+
+
